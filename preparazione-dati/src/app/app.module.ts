11cee7e49ef3fc2db4459cf2690006c168e5cbb7
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-01-14 11:31:52 +0000

    punto 3

--- preparazione-dati/src/app/app.module.ts
+++ preparazione-dati/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { MacchineComponent } from './macchine/macchine.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    MacchineComponent
   ],
   imports: [
     BrowserModule
---
+++ preparazione-dati/src/app/macchine/macchine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-macchine',
+  templateUrl: './macchine.component.html',
+  styleUrls: ['./macchine.component.css']
+})
+export class MacchineComponent {
+
+}
